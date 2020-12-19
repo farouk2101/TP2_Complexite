bf49421e3baa8eb11e53e2560e6424cda9ceb48a
sykom1 <saber.tnifas@etu.univ-amu.fr>
2020-12-19 16:53:12 +0100

    push debut du 2

--- src/MiniProjet_2/Main.java
+++ src/MiniProjet_2/Main.java
@@ -2,6 +2,15 @@ package MiniProjet_2;
 
 public class Main {
 
-    
+    public static void main(String[] args) {
+        Graph graph = new Graph("./ressources/graph.txt", false);
+        System.out.println(graph);
+    }
+
+
+
+
+
+
 
 }
